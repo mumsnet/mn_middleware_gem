9958164e1b86a62f68b68d150048c9e67708c23a
Shamim Mirzai <sham@mumsnet.com>
2019-04-03 17:48:48 +0100

    update and store remote ip

--- build_gem.sh
+++ build_gem.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+if [ ! -f Gemfile.lock ]; then
+  touch Gemfile.lock
+fi
+if [ ! -f .env ]; then
+  touch .env
+fi
+
 docker-compose build
 docker-compose run test bundle
 docker-compose run test bundle exec rake install
--- lib/mn_middleware_gem/remote_ip_logger.rb
+++ lib/mn_middleware_gem/remote_ip_logger.rb
@@ -1,3 +1,5 @@
+require 'request_store'
+
 module MnMiddleware
   class RemoteIpLogger
     def initialize(app)
@@ -8,6 +10,7 @@ module MnMiddleware
       if env["HTTP_X_FORWARDED_FOR"]
         remote_ip = env["HTTP_X_FORWARDED_FOR"].split(",")[0]
         env['REMOTE_ADDR'] = env["action_dispatch.remote_ip"] = env["HTTP_X_FORWARDED_FOR"] = remote_ip
+        RequestStore.store[:remote_ip] = remote_ip
         @app.call(env)
       else
         @app.call(env)
